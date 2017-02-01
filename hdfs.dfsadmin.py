ad2548abc78ed19295eb10cefd7c54e8a9e51988
System <Mr.NN91@gmail.com>
2017-02-02 00:19:06 +0700

    Update hdfs.dfsadmin.py

--- hdfs.dfsadmin.py
+++ hdfs.dfsadmin.py
@@ -1,3 +1,54 @@
 
 
 # http://fibrevillage.com/storage/631-hadoop-dfsadmin-command-examples
+
+
+
+# dfsAdmin Command
+
+
+# dfsadmin -report, report HDFS statistics
+hdfs dfsadmin -report
+
+
+# dfsadmin -safemode maintenance command
+
+
+
+# hdfs dfsadmin -saveNamespace
+
+
+# hdfs dfsadmin -rollEdits
+
+
+# hdfs dfsadmin -restoreFailedStorage
+
+
+# hdfs dfsadmin -refreshNode
+
+
+# hdfs dfsadmin -reconfig datanode
+
+
+# hdfs dfsadmin -setBalancerBandwidth
+
+
+# hdfs dfsadmin -allowSnapshot/disallowSnapshot
+
+
+# hdfs dfsadmin -fetchImage
+
+
+# hdfs dfsadmin -getDatanodeInfo
+
+
+# hdfs dfsadmin -shutdownDatanode
+
+
+# hdfs dfsadmin -triggerBlockReport
+
+
+# hdfs dfsadmin -printTopology
+
+
+
