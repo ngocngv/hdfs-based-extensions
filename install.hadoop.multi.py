c6017bd6330e783a554e2b7254996b81b75c5608
System <Mr.NN91@gmail.com>
2017-02-01 22:24:37 +0700

    Update and rename install.hadoop.mutil.py to install.hadoop.multi.py

---
+++ install.hadoop.multi.py
@@ -0,0 +1,15 @@
+
+# https://github.com/kiwenlau/hadoop-cluster-docker
+# http://user501254.github.io/BD_STTP_2016/hadoop-multi-node-cluster-installation-guide/
+# http://fibrevillage.com/storage/617-hadoop-2-7-cluster-installation-and-configuration-on-rhel7-centos7  
+# http://www.tecmint.com/install-configure-apache-hadoop-centos-7/
+
+
+
+
+
+
+
+
+
+
