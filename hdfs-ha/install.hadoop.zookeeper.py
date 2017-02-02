19d95c4fbc2c95c790c4196c30e8a507eed2be2f
System <Mr.NN91@gmail.com>
2017-02-02 12:32:57 +0700

    Update install.hadoop.zookeeper.py

--- hdfs-ha/install.hadoop.zookeeper.py
+++ hdfs-ha/install.hadoop.zookeeper.py
@@ -9,10 +9,10 @@
 
 
 
-10.10.0.1      dn1.hdfs.cluster
+10.10.0.1      dn1.hdfs
 
-10.10.0.2      nn1.hdfs.cluster   dn2.rad coord1 gtm1 rm1.rad
-10.10.0.3      nn2.hdfs.cluster   dn3.rad coord2 gtm2 rm2.rad
+10.10.0.2      nn1.hdfs   dn2.hdfs   coord1 gtm1 rm1.rad
+10.10.0.3      nn2.hdfs   dn3.hdfs   coord2 gtm2 rm2.rad
 
 
 
