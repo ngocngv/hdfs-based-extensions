8623542c3ee3eaf552c268773ea8a2bcf98135ca
System <Mr.NN91@gmail.com>
2017-02-02 23:51:26 +0700

    Update install.hdfs2.py

--- hdfs/install.hdfs2.py
+++ hdfs/install.hdfs2.py
@@ -80,13 +80,35 @@ chown -R hadoop:hadoop /opt/hadoop/
 
   
   
+#
+# Setup Environment Variables   
+#------------------------------------------------------------------------------  
+su - hadoop  
+# edit ~/.bashrc  |  ~/.bash_profile
   
-  
-  
-  
-  
-  
-  
+
+## JAVA env variables
+export JAVA_HOME=/usr/java/default
+export PATH=$PATH:$JAVA_HOME/bin
+export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
+## HADOOP env variables
+export HADOOP_HOME=/opt/hadoop
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_YARN_HOME=$HADOOP_HOME
+export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native"
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
+#
+export HADOOP_INSTALL=$HADOOP_HOME
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+#
+export YARN_HOME=$HADOOP_HOME
+export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop
+#
+export ZOOKEEPER_HOME=/opt/zookeeper
+export PATH=$PATH:$JAVA_HOME/bin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$ZOOKEEPER_HOME/bin 
   
   
   
