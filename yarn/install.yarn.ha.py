b4e334fc30f436c251a792567260936d228f25aa
System <Mr.NN91@gmail.com>
2017-02-02 14:45:10 +0700

    Update install.yarn.ha.py

--- yarn/install.yarn.ha.py
+++ yarn/install.yarn.ha.py
@@ -4,4 +4,18 @@
 
 
 
+# https://www.edureka.co/blog/interview-questions/hadoop-interview-questions-hadoop-cluster/?utm_source=blog&utm_medium=left-menu&utm_term=Hadoop%20Interview%20Questions%20%E2%80%93%20Setting%20Up%20Hadoop%20Cluster
 
+
+/sbin/start-yarn.sh
+
+./sbin/yarn-daemon.sh start resourcemanager
+./sbin/yarn-daemon.sh start nodemanager
+
+
+
+./sbin/hadoop-daemon.sh start namenode
+./sbin/hadoop-daemon.sh start datanode
+./sbin/yarn-daemon.sh start resourcemanager
+./sbin/yarn-daemon.sh start nodemanager
+./sbin/mr-jobhistory-daemon.sh start historyserver
