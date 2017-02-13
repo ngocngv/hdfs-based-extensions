31e98f6f62497c03bfe309f8ce6511550e4f6518
System <Mr.NN91@gmail.com>
2017-02-13 14:07:02 +0700

    Update and rename spark.submit.py to run.spark.submit.py

--- spark/run.spark.submit.py
+++ spark/run.spark.submit.py
@@ -43,9 +43,9 @@
     ./examples/jars/spark-examples_2.11-2.1.0.jar \
     1000
 
-  
-  
-  
+
+
+
 # Run on a Spark standalone cluster in cluster deploy mode with supervise
 ./bin/spark-submit \
     --class org.apache.spark.examples.SparkPi \
@@ -56,13 +56,13 @@
     --total-executor-cores 4 \
     ./examples/jars/spark-examples_2.11-2.1.0.jar \
     1000
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
 # Run on a YARN cluster
 #-------------------------------------------------------------------------------------
 
@@ -77,12 +77,12 @@ export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
     --num-executors 50 \
     ./examples/jars/spark-examples_2.11-2.1.0.jar \
     1000
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
 # Run a Python application on a Spark standalone cluster
 #-------------------------------------------------------------------------------------
 
@@ -90,27 +90,47 @@ export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
     --master spark://yarn1.local:7077 \
     ./examples/src/main/python/pi.py \
     1000
-  
-  
-  
+
+
+
 # Run on a Mesos cluster in cluster deploy mode with supervise
 ./bin/spark-submit \
     --class org.apache.spark.examples.SparkPi \
-    --master spark://yarn1.local:7077 \
+    --master mesos://yarn1.local:7077 \
     --deploy-mode cluster \
     --supervise \
     --executor-memory 2G \
     --total-executor-cores 4 \
     http://path/to/examples.jar \
     1000
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+    
+    
+    
+    
+    
+    
+    
+# Master URLs
+#-------------------------------------------------------------------------------------
+
+spark://HOST:PORT           # Connect to the given Spark standalone cluster master. 
+                            # The port must be whichever one your master is configured to use, which is 7077 by default.
+
+mesos://HOST:PORT           # Connect to the given Mesos cluster. 
+                            # The port must be whichever one your is configured to use, which is 5050 by default. 
+                            # Or, for a Mesos cluster using ZooKeeper, use mesos://zk://.... To submit with --deploy-mode cluster, the HOST:PORT should be configured to connect to the MesosClusterDispatcher.
+                
+yarn                        # Connect to a YARN cluster in client or cluster mode depending on the value of --deploy-mode. 
+                            # The cluster location will be found based on the HADOOP_CONF_DIR or YARN_CONF_DIR variable.
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
